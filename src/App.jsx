085ecb88d6240005089ed62e714f519d40505d42
Rahida Rashidli <reshidlirahide@gmail.com>
2025-02-28 13:31:03 +0400

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,47 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useLocation,
+  Outlet,
+} from "react-router-dom";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Contact from "./pages/Contact/Contact";
 import BuyNow from "./pages/BuyNow/BuyNow";
 import Navbar from "./shared/Navbar/Navbar";
 import Footer from "./shared/Footer/Footer";
+import NotFound from "./components/NotFound/NotFound";
+
+const Layout = () => {
+  const location = useLocation();
+  const validRoutes = ["/", "/about", "/contact", "/buy"];
+  const showLayout = validRoutes.some((route) =>
+    location.pathname.startsWith(route)
+  );
+
+  return (
+    <>
+      {showLayout && <Navbar />}
+      <Outlet />
+      {showLayout && <Footer />}
+    </>
+  );
+};
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Navbar />
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/about" element={<About />}></Route>
-        <Route path="/contact" element={<Contact />}></Route>
-        <Route path="/buy" element={<BuyNow />}></Route>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/buy" element={<BuyNow />} />
+        </Route>
+        <Route path="*" element={<NotFound />}></Route>
       </Routes>
-      <Footer />
     </BrowserRouter>
   );
 };
--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,8 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./NotFound.css";
 
 const NotFound = () => {
-  return <div></div>;
+  return (
+    <div className="not-found-container">
+      <div className="not-found-content">
+        <h1 className="not-found-title">404</h1>
+        <div className="smile">😞</div>
+        <p className="not-found-message">
+          Oops! The page you are looking for does not exist.
+        </p>
+        <Link to="/" className="back-home-button">
+          Go to Home
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
